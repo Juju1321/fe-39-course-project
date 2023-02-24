407ba5bcd414f9a80e60247487b4789b7f09a70e
juju1321 <venera_2022@mail.ru>
2023-02-24 13:13:51 +0300

    add component Card

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,6 @@
 export { CloseIcon } from "./CloseIcon";
 export { OpenedMenu } from "./OpenedMenu";
+export { MoreIcon } from "./MoreIcon";
+export { BookmarkIcon } from "./BookmarkIcon";
+export { LikeIcon } from "./LikeIcon";
+export { DislikeIcon } from "./DislikeIcon";
